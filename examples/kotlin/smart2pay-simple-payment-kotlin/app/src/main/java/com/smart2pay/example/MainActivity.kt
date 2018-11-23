fc304c45db48db07f2bb7e3150365f57341a3430
David Jupijn <david@risingstep.com>
2018-11-23 13:16:19 +0100

    Add Credit Card handling to Main Acitivity

--- examples/kotlin/smart2pay-simple-payment-kotlin/app/src/main/java/com/smart2pay/example/MainActivity.kt
+++ examples/kotlin/smart2pay-simple-payment-kotlin/app/src/main/java/com/smart2pay/example/MainActivity.kt
@@ -6,10 +6,12 @@ import android.util.Log
 import android.widget.Button
 import com.smart2pay.example.models.Order
 import com.smart2pay.example.requests.RequestManager
+import com.smart2pay.example.requests.requests.AuthorizationApiKeyRequest
 import com.smart2pay.example.requests.requests.PaymentsRequest
 import com.smart2pay.example.requests.requests.PaymentsVerifyRequest
 import com.smart2pay.sdk.PaymentManager
 import com.smart2pay.sdk.models.Payment
+import com.smart2pay.sdk.requests.requests.CardAuthenticationRequest
 
 class MainActivity : AppCompatActivity(), PaymentManager.PaymentManagerEventListener {
     val TAG = "SMART2PAY"
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity(), PaymentManager.PaymentManagerEventList
 
         val alipayButton = this.findViewById<Button>(R.id.alipay_button)
         val wechatButton = this.findViewById<Button>(R.id.wechat_button)
+        val creditCardButton = this.findViewById<Button>(R.id.creditcard_button)
 
         alipayButton!!.setOnClickListener {
             pay(Payment.PaymentProvider.ALIPAY)
@@ -28,6 +31,10 @@ class MainActivity : AppCompatActivity(), PaymentManager.PaymentManagerEventList
 
         wechatButton!!.setOnClickListener {
             pay(Payment.PaymentProvider.WECHAT)
+        }
+
+        creditCardButton!!.setOnClickListener {
+            getApiKeyForCreditCardCheck()
         }
     }
 
@@ -82,6 +89,89 @@ class MainActivity : AppCompatActivity(), PaymentManager.PaymentManagerEventList
                     }
                 }
         paymentsVerifyRequest.enqueue()
+    }
+
+    // Credit Card handling
+
+    private fun getApiKeyForCreditCardCheck() {
+        val authorizationApiKeyRequest = AuthorizationApiKeyRequest(RequestManager.instance)
+        authorizationApiKeyRequest.callback =
+                object : AuthorizationApiKeyRequest.Callback {
+                    override fun onSuccess(apiKey: String) {
+                        // Authorization was successful!
+                        Log.d("ApiTokenForCreditCard", apiKey)
+                        getCreditCardToken(apiKey)
+                    }
+
+                    override fun onFailure() {
+                        // Authorization was a failure!
+                    }
+                }
+        authorizationApiKeyRequest.enqueue()
+    }
+
+    private fun getCreditCardToken(apiKey: String) {
+//        {
+//            "CardAuthentication": {
+//                "Customer": {
+//                    "FirstName": "John",
+//                    "LastName": "Doe",
+//                    "Email": "testing2@test.com",
+//                    "SocialSecurityNumber": "00003456789"
+//                },
+//                "BillingAddress": {
+//                    "Country": "BR"
+//                },
+//                "Card": {
+//                    "HolderName": "John Doe",
+//                    "Number": "4111111111111111",
+//                    "ExpirationMonth": "02",
+//                    "ExpirationYear": "2019",
+//                    "SecurityCode": "312"
+//                }
+//            }
+//        }
+
+        val body = HashMap<String, Any>()
+        val cardAuthentication = HashMap<String, Any>()
+        val customer = HashMap<String, Any>()
+        val billingAddress = HashMap<String, Any>()
+        val card = HashMap<String, Any>()
+
+        customer["FirstName"] = "John"
+        customer["LastName"] = "Doe"
+        customer["Email"] = "android@sdktest.com"
+        customer["SocialSecurityNumber"] = "00003456789"
+
+        billingAddress["Country"] = "BR"
+
+        card["HolderName"] = "John Doe"
+        card["Number"] = "4111111111111111"
+        card["ExpirationMonth"] = "02"
+        card["ExpirationYear"] = "2019"
+        card["SecurityCode"] = "321"
+
+        cardAuthentication["Customer"] = customer
+        cardAuthentication["BillingAddress"] = billingAddress
+        cardAuthentication["Card"] = card
+
+        body["CardAuthentication"] = cardAuthentication
+
+        val cardAuthenticationRequest = CardAuthenticationRequest("Basic $apiKey", true)
+        cardAuthenticationRequest.setRequestBody(body)
+        cardAuthenticationRequest.callback =
+                object : CardAuthenticationRequest.Callback {
+                    override fun onSuccess(creditCardToken: String) {
+                        // Verification was successful!
+                        // Send it to your server and initiate a transactions via REST API: https://docs.smart2pay.com/category/direct-card-processing/one-click-payment/
+                        Log.d("TokenForCreditCard", creditCardToken)
+                    }
+
+                    override fun onFailure() {
+                        // Verification was a failure!
+                    }
+                }
+        cardAuthenticationRequest.enqueue()
     }
 
     // PaymentManagerEventListener callbacks
