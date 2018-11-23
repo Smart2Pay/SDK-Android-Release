fd1f28d754ac2e589c8bc62d4da28be71ba98382
David Jupijn <david@risingstep.com>
2018-11-23 14:26:09 +0100

    Add Credit Card button and click listener

--- examples/java/smart2pay-simple-payment-java/app/src/main/java/com/smart2pay/example/MainActivity.java
+++ examples/java/smart2pay-simple-payment-java/app/src/main/java/com/smart2pay/example/MainActivity.java
@@ -1,19 +1,16 @@
 package com.smart2pay.example;
 
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.content.DialogInterface;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.smart2pay.example.models.Order;
 import com.smart2pay.example.requests.RequestManager;
@@ -37,16 +34,24 @@ public class MainActivity extends AppCompatActivity implements PaymentManager.Pa
         setContentView(R.layout.activity_main);
 
         Button alipayButton = findViewById(R.id.alipay_button);
-        Button wechatButton = findViewById(R.id.wechat_button);
+        Button weChatButton = findViewById(R.id.wechat_button);
+        Button creditCardButton = findViewById(R.id.creditcard_button);
+
         alipayButton.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
                 pay(Payment.PaymentProvider.ALIPAY);
             }
         });
 
-        wechatButton.setOnClickListener( new View.OnClickListener() {
+        weChatButton.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
                 pay(Payment.PaymentProvider.WECHAT);
+            }
+        });
+
+        creditCardButton.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+
             }
         });
     }
@@ -56,7 +61,6 @@ public class MainActivity extends AppCompatActivity implements PaymentManager.Pa
         order.setAmount(Integer.parseInt(((EditText)findViewById(R.id.e_amount)).getText().toString()));
         order.setCurrency(((EditText)findViewById(R.id.e_currency)).getText().toString());
         order.setType(paymentProvider);
-        RequestManager.Companion.initialize(this);
         this.placeOrder(order);
     }
 
@@ -111,6 +115,9 @@ public class MainActivity extends AppCompatActivity implements PaymentManager.Pa
         }));
         paymentsVerifyRequest.enqueue();
     }
+
+    // Credit Card handling
+
 
 
     // PaymentManagerEventListener callbacks
