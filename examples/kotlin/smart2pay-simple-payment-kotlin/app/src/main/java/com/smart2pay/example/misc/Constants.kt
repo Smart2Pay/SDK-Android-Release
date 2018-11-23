e4af9edb5c2124784e25f70414ed7c6e81c0ba19
David Jupijn <david@risingstep.com>
2018-11-23 14:56:11 +0100

    Add Dummy credit card data to Constants

--- examples/kotlin/smart2pay-simple-payment-kotlin/app/src/main/java/com/smart2pay/example/misc/Constants.kt
+++ examples/kotlin/smart2pay-simple-payment-kotlin/app/src/main/java/com/smart2pay/example/misc/Constants.kt
@@ -6,4 +6,33 @@ object Constants {
     const val apiUrl = "https://s2pmerchantserver.azurewebsites.net"
     const val apiToken = "Basic UzJwTWVyY2hhbnRTZXJ2ZXI6UzJwTWVyY2hhbnRTZXJ2ZXJQYXNzd29yZA=="
     val isDebug = BuildConfig.DEBUG
+
+    fun dummyCreditCardData() : HashMap<String, Any> {
+        val body = HashMap<String, Any>()
+        val cardAuthentication = HashMap<String, Any>()
+        val customer = HashMap<String, Any>()
+        val billingAddress = HashMap<String, Any>()
+        val card = HashMap<String, Any>()
+
+        customer["FirstName"] = "John"
+        customer["LastName"] = "Doe"
+        customer["Email"] = "android@sdktest.com"
+        customer["SocialSecurityNumber"] = "00003456789"
+
+        billingAddress["Country"] = "BR"
+
+        card["HolderName"] = "John Doe"
+        card["Number"] = "4111111111111111"
+        card["ExpirationMonth"] = "02"
+        card["ExpirationYear"] = "2019"
+        card["SecurityCode"] = "321"
+
+        cardAuthentication["Customer"] = customer
+        cardAuthentication["BillingAddress"] = billingAddress
+        cardAuthentication["Card"] = card
+
+        body["CardAuthentication"] = cardAuthentication
+
+        return body
+    }
 }
