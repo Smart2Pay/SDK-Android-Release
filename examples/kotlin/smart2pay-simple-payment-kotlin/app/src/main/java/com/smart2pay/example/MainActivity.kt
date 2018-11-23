dbc4abb99552d95419e7ba2b67ad49922923947a
David Jupijn <david@risingstep.com>
2018-11-23 14:59:33 +0100

    Use Dummy data from Constants kotlin

--- examples/kotlin/smart2pay-simple-payment-kotlin/app/src/main/java/com/smart2pay/example/MainActivity.kt
+++ examples/kotlin/smart2pay-simple-payment-kotlin/app/src/main/java/com/smart2pay/example/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
+import com.smart2pay.example.misc.Constants
 import com.smart2pay.example.models.Order
 import com.smart2pay.example.requests.RequestManager
 import com.smart2pay.example.requests.requests.AuthorizationApiKeyRequest
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity(), PaymentManager.PaymentManagerEventList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RequestManager.initialize(this@MainActivity)
 
         val alipayButton = this.findViewById<Button>(R.id.alipay_button)
         val wechatButton = this.findViewById<Button>(R.id.wechat_button)
@@ -132,33 +135,8 @@ class MainActivity : AppCompatActivity(), PaymentManager.PaymentManagerEventList
 //            }
 //        }
 
-        val body = HashMap<String, Any>()
-        val cardAuthentication = HashMap<String, Any>()
-        val customer = HashMap<String, Any>()
-        val billingAddress = HashMap<String, Any>()
-        val card = HashMap<String, Any>()
-
-        customer["FirstName"] = "John"
-        customer["LastName"] = "Doe"
-        customer["Email"] = "android@sdktest.com"
-        customer["SocialSecurityNumber"] = "00003456789"
-
-        billingAddress["Country"] = "BR"
-
-        card["HolderName"] = "John Doe"
-        card["Number"] = "4111111111111111"
-        card["ExpirationMonth"] = "02"
-        card["ExpirationYear"] = "2019"
-        card["SecurityCode"] = "321"
-
-        cardAuthentication["Customer"] = customer
-        cardAuthentication["BillingAddress"] = billingAddress
-        cardAuthentication["Card"] = card
-
-        body["CardAuthentication"] = cardAuthentication
-
         val cardAuthenticationRequest = CardAuthenticationRequest("Basic $apiKey", true)
-        cardAuthenticationRequest.setRequestBody(body)
+        cardAuthenticationRequest.setRequestBody(Constants.dummyCreditCardData())
         cardAuthenticationRequest.callback =
                 object : CardAuthenticationRequest.Callback {
                     override fun onSuccess(creditCardToken: String) {
